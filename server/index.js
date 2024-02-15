0983ce815b59a5a3c6b8a45318b7e144a06cd6f4
babias <ababias01@gmail.com>
2024-02-15 16:05:03 -0500

    por

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const app =express()
+const express = require('express');
+const app = express();
 
 app.use("/", (req, res) => {
   res.send("Server is running.");
 });
 
-app.listen(5000, console.log("server started on port 5000"))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
