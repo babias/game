6456c7fe8a9641cdab55eedf1a4c060f95e9cca7
babias <ababias01@gmail.com>
2024-02-15 15:12:46 -0500

    chat room added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+// import TextAdventureGame from './components/TextAdventureGame';
+import ChatRoom from './components/ChatRoom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Escape C13, your future depends on it!</h3>
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <ChatRoom />
+          {/* <TextAdventureGame /> */}
+        </div>      
+      </div>
     </div>
   );
-}
+};
 
 export default App;
